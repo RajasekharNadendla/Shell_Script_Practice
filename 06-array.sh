cd3e749ed1886ec52dede525aa52d11c81ee6019
Rajasekhar <rajasekhar15052003@gmail.com>
2025-04-17 20:50:34 +0530

    this is my commit

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+arr=("apple" "banana" "orange")
+echo "array at index 0 is : arr[$0]"
+echo "array at index 1 is : arr[$1]"
+echo "array at index 2 is : arr[$2]"
+echo "all array elements are : arr[$@]"
+
