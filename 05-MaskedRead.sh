ada92f72e02d8ba15cc8ddafe5e18294d0e5c7d1
Rajasekhar <rajasekhar15052003@gmail.com>
2025-04-17 20:45:44 +0530

    this is my commit

---
+++ 05-MaskedRead.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "enter the username ::"
+read -s USERNAME
+echo "enter the password ::"
+read -s PASSWORD
+echo "my username is : $USERNAME"
+echo "my password is : $PASSWORD"
+
+
