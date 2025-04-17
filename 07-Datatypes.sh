61d636a5fc69ba35494bf374682e113fb5c713c4
Rajasekhar <rajasekhar15052003@gmail.com>
2025-04-17 21:01:29 +0530

    this is my commit

---
+++ 07-Datatypes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+num1=$1
+num2=$2
+sum=$((num1 + num2))
+DifferentwaySum=$(($num1 + $num2))
+
+echo "the value of sum is : $sum"
+echo "the value of DifferentwaySum is : $DifferentwaySum"
