59bbc019baa38c85d010b9be85ff7f7737b0b22c
Rajasekhar <rajasekhar15052003@gmail.com>
2025-04-17 22:02:52 +0530

    this is my commit

--- 09-condition.sh
+++ 09-condition.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 var1=$1
-if $val1 -gt 10; then 
-    echo "the input is greater than 10"
+if $var1 -gt 10; then 
+    echo "the input $var1 is greater than 10"
 else 
-    echo "the input is less than 10"
+    echo "the input $var1 is less than 10"
 fi
