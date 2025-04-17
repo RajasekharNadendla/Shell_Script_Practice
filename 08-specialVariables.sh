2b9068886c6e658e726e436aaee3dc8de351fe75
Rajasekhar <rajasekhar15052003@gmail.com>
2025-04-17 21:08:48 +0530

    this is my commit

---
+++ 08-specialVariables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "to get all the variables : $@"
+echo "to get number of variables : $#"
+echo "to get current script file name : $0"
+echo "to get the previous command is passed/failed(exit status) : $?"
+echo "to get the home directory : $home"
+echo "te get the name of the user who is running th script : $user"
