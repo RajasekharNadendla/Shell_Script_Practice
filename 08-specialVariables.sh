3ecde6c21bce63cfad158008f59bb3cf1509ff4a
Rajasekhar <rajasekhar15052003@gmail.com>
2025-04-17 21:21:38 +0530

    this is my commit

--- 08-specialVariables.sh
+++ 08-specialVariables.sh
@@ -7,4 +7,7 @@ echo "to get the previous command is passed/failed(exit status) : $?"
 echo "to get the home directory                                 : $HOME"
 echo "te get the name of the user who is running th script      : $USER"
 echo "to get the current working directory                      : $PWD"
+echo "to get the name of the host                               : $HOSTNAME"
 echo "to get the process id of current running file             : $$"
+sleep 20 &
+echo "to get the process id of the last background command      : $!"
