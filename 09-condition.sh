5359a6c557dc832a349bce8758297356b819ab57
Rajasekhar <rajasekhar15052003@gmail.com>
2025-04-18 21:30:23 +0530

    this is my commit

--- 09-condition.sh
+++ 09-condition.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 var1=$1
-if [ "$var1" -gt 10 ]; then 
+if [ "$var1" -gt 10 ]; then #the syntax after/before ending the square brackets space and variable should be in the "<variable>"
     echo "the input $var1 is greater than 10"
 else 
     echo "the input $var1 is less than 10"
---
+++ 10-InstallationInServer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0] then
+    echo "please shift to the root user"
+else 
+    echo "You are in the root access"
+fi
+
+dnf install mysqld -y
+
+if [ $? -eq 0 ]
+then 
+    echo "Mysql is done success"
+else 
+    echo "mysql installation is unsuccess"
+fi
