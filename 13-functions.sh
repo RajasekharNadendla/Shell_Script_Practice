2505fd774e8e8d64e09ad27bdeb97a65fc128446
Rajasekhar <rajasekhar15052003@gmail.com>
2025-04-19 22:51:49 +0530

    loops

---
+++ 13-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+TimeStamp=$( date +%F-%H-%M-%S )
+scriptName=$( echo $0 | cut -d "." -f1 )
+logfile=/tmp/$scriptName-$TimeStamp.log
+
+
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+validate(){
+    if [ $1 -ne 0]
+    then
+        echo -e "$2 is .. $R failed $N"
+        exit 1
+    else
+        echo -e "$2 is .. $R Success $N"
+}
+
+
+pwd &>>logfile
+validate $? "checking the current working directory"
+
+sl -sa &>>logfile
+validate $? "Checking the List command"
