452bd4702c9f56ef73f4c211b35f076ba2bafe79
Rajasekhar <rajasekhar15052003@gmail.com>
2025-04-23 22:21:18 +0530

    extra info in scripting

--- 14-CallwithOutSource.sh
+++ 14-CallwithOutSource.sh
@@ -5,6 +5,6 @@ Course="I am learning devops"
 echo "this is from current file : $Course"
 echo "this is the current file process id : $$"
 
-./15-otherfile.sh
+./16-otherfile.sh
 
 echo "this is after calling the other file :$Course"
